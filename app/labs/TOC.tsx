9f3ee0e7a7d355955f26a233fad93b9a1a6417a6
Daniel McCue <danielmccue1@icloud.com>
2026-02-08 19:04:29 -0500

    my github

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -22,7 +22,7 @@ export default function TOC() {
         <NavLink href="/" as={Link}>Kambaz</NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+        <NavLink id="wd-github" href="https://github.com/DanMccue/CS4450-A1">My GitHub</NavLink>
       </NavItem>
     </Nav>
   );
